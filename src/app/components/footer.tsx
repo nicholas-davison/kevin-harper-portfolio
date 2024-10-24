f714253a24a39f4d5d56533bb41f301df55494ac
Nicholas Davison <nick.a.davison@gmail.com>
2024-10-24 16:43:56 -0500

    changed and resized font, added conditional parallax to background images

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,13 +1,27 @@
-
 export const Footer = () => {
-
-    return (
-        <div className="flex justify-center items-center h-36 text-white bg-slate-950">
-            <p><i className="fa-regular fa-copyright"></i> 2024 Kevin Harper - All Rights Reserved  |  Site by
-                <a href="https://portfolio-nicholas-davisons-projects.vercel.app/" target="_blank" rel="noopener noreferrer" className="ml-1">
-                     Nick Davison
-                </a>
-            </p>
-        </div>
-    )
-} 
+  return (
+    <div className="flex justify-center items-center h-36 text-white bg-slate-950">
+      <h1 className="flex flex-wrap sm:flex-col justify-center items-center text-center">
+        <span className="mr-2">
+          <i className="fa-regular fa-copyright"></i> 2024 Kevin Harper - All Rights Reserved 
+        </span>
+        <span className="ml-2 flex text-center items-center">
+          Site by
+          <a
+            href="https://portfolio-nicholas-davisons-projects.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ml-2 flex items-center"
+          >
+            <img
+              src="/NDLogo.png"
+              className="h-8 mr-2"
+              alt="Nicholas Davison Logo"
+            /> 
+            Nick Davison
+          </a>
+        </span>
+      </h1>
+    </div>
+  );
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,9 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Lexend+Zetta:wght@100..900&family=Nobile:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
