daac1194ccc64be91aa9303683bcc5ef6d3224c9
Nicholas Davison <nick.a.davison@gmail.com>
2024-11-05 11:25:02 -0600

    added allcredits, need to refine. also parallax for album art, hover for contact icons, and hover for album art

--- src/app/components/albumArt.tsx
+++ src/app/components/albumArt.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react";
+import { getAlbums } from "../services/spotify";
 
 export default function AlbumArt() {
 
@@ -8,32 +9,15 @@ export default function AlbumArt() {
 
     useEffect(() => {
       const fetchAlbums = async () => {
-        // Fetch token from the Next.js API route
-        const tokenResponse = await fetch('/api/spotify-token');
-        const { access_token } = await tokenResponse.json();
-        
         const albumIds = [
-            "21PZ6vS8uahnrZmySYDh3K",
-            "5n8ppNRmmYvkFGnQZPHdNc",
-            "5GWW6Tsj1ojzdTfIWYtLmA",
-            "6TJMdIq1xmVFOFoWrBDAU1",
-            "5XlYVBaXjtBJjUQafmNqC8",
-            "13vdgL9Dnx1HPsAoblDmod",
-            "2vxiffHHOTPAbhRiAn44B8",
-            "01PVvtU85NR2QJAlpwUKD5",
-            "0tAqHptInfhc8HKYSTWEMp",
-            "13LYdyvwNgwiiZH54nQbiE",
-            "6L8iHimSAekApvL7gERFt5",
-            "05G8lvi4ziqGao6fCfxMUO"
-        ]
-        // Fetch album data from Spotify
-        const albumResponse = await fetch(`https://api.spotify.com/v1/albums?ids=${albumIds}`, {
-          headers: {
-            Authorization: `Bearer ${access_token}`,
-          },
-        });
-        const albumData = await albumResponse.json();
-        setAlbums(albumData.albums);
+          "21PZ6vS8uahnrZmySYDh3K", "5n8ppNRmmYvkFGnQZPHdNc", "5GWW6Tsj1ojzdTfIWYtLmA",
+          "6TJMdIq1xmVFOFoWrBDAU1", "5XlYVBaXjtBJjUQafmNqC8", "13vdgL9Dnx1HPsAoblDmod",
+          "2vxiffHHOTPAbhRiAn44B8", "01PVvtU85NR2QJAlpwUKD5", "0tAqHptInfhc8HKYSTWEMp",
+          "13LYdyvwNgwiiZH54nQbiE", "6L8iHimSAekApvL7gERFt5", "05G8lvi4ziqGao6fCfxMUO"
+        ];
+  
+        const albumData = await getAlbums(albumIds);
+        if (albumData) setAlbums(albumData);
       };
   
       fetchAlbums();
@@ -41,32 +25,21 @@ export default function AlbumArt() {
     
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
-          {albums.map((album) => (
-            <div key={album.id} className="w-70 h-70">
-              <img src={album.images[0].url} alt={album.name} className="album-cover" />
+          {albums && albums.map((album) => (
+            <div key={album.id} className="relative w-70 h-70 group">
+            <img src={album.images[0].url} alt={album.name} className="album-cover w-full h-full object-cover" />
+            
+            {/* This div will show only on hover */}
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <p className="text-white text-lg font-semibold">{album.artists[0].name}</p>
             </div>
+          </div>
+/*             <div key={album.id} className="w-70 h-70">
+              <img src={album.images[0].url} alt={album.name} className="album-cover" />
+            </div> */
           ))}
         </div>
       );
     };
-    
-/*     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
-            <div className="bg-gray-800 w-60 h-60 rounded-md"></div>
-            <div className="bg-gray-800 w-60 h-60 rounded-md"></div>
-            <div className="bg-gray-800 w-60 h-60 rounded-md"></div>
-            <div className="bg-gray-800 w-60 h-60 rounded-md"></div>
-            
-            <div className="bg-gray-800 w-60 h-60 rounded-md"></div>
-            <div className="bg-gray-800 w-60 h-60 rounded-md"></div>
-            <div className="bg-gray-800 w-60 h-60 rounded-md"></div>
-            <div className="bg-gray-800 w-60 h-60 rounded-md"></div>
-            
-            <div className="bg-gray-800 w-60 h-60 rounded-md"></div>
-            <div className="bg-gray-800 w-60 h-60 rounded-md"></div>
-            <div className="bg-gray-800 w-60 h-60 rounded-md"></div>
-            <div className="bg-gray-800 w-60 h-60 rounded-md"></div>
-        </div>
-    ) 
-} */
+
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,6 +27,7 @@ export default function Home() {
 
   return (
     <div>
+      {/* Cover Photo */}
       <Parallax imageUrl="/K4.jpeg" speed={0.01} cover={true}>
         <ul className="text-white text-3xl md:text-6xl mt-[-20px] md:mt-[-50px] ml-6 md:ml-16 bg-black bg-opacity-50 p-4 rounded-lg">
           <li className='mb-2'>Production</li>
@@ -34,6 +35,8 @@ export default function Home() {
           <li className='my-2'>Mixing</li>
         </ul>
       </Parallax>
+
+      {/* About */}
       <div 
         id="about"
         className="flex flex-col h-100 scroll-mt-24"
@@ -56,6 +59,8 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* Album Art */}
       <div className="h-auto bg-black flex flex-col items-center justify-center">
           <AlbumArt/>
           <Link href="/credits">
@@ -67,13 +72,15 @@ export default function Home() {
             </button>
           </Link>
       </div>
-      {/* <Parallax imageUrl="/lines.jpg" speed={0.01} cover={false}> */}
-      <div 
+      <Parallax imageUrl="/lines.jpg" speed={0.015} cover={false}>
+
+      {/* Spotify Players */}
+{/*       <div 
           className='h-auto p-16 bg-cover'    
            style={{ 
             backgroundImage: 'url(/lines.jpg)',
             backgroundPosition: 'center 20%' 
-        }}>
+        }}> */}
           <iframe 
             style={{borderRadius:"12px"}}
             src="https://open.spotify.com/embed/track/6y9k1hTsiqA2zlpxhlGZrH?utm_source=generator&theme=0"
@@ -101,97 +108,8 @@ export default function Home() {
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
             loading="lazy"
             />
-      </div>
-      {/* </Parallax> */}
+      {/* </div> */}
+      </Parallax>
     </div>
   );
 }
-
-/*     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          > 
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div> */
